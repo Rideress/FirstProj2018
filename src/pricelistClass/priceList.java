51662ef65f1f6a6b5aea8a21d21aa76d83a12f2b
roman <phillroman24@gmail.com>
2018-03-14 22:24:49 +0300

    Коммит для создания проекта на гитхабе

---
+++ src/pricelistClass/priceList.java
@@ -0,0 +1,18 @@
+package pricelistClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+ public class priceList {
+     private Map<String,String> priceList = new HashMap<>();
+
+     /**
+      * @param name - name of a code.
+      * @param price - price of a code.
+      *
+      */
+     private void addGood(String name,String price){
+
+     }
+
+ }
